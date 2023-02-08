1c14a35216130c6428a8635407a270c9bbfa9e26
Sreekanth Nagareddy <sreeisalso@gmail.com>
2023-02-08 11:26:16 +0530

    Added c8 code coverage

--- index.test.ts
+++ index.test.ts
@@ -2,13 +2,11 @@ import { amountToWords, numberToWords } from "./index";
 import { test, expect } from "vitest";
 
 test("123", () => {
-  expect(numberToWords("123")).toBe("One Hundred Twenty Three");
+  expect(numberToWords(123)).toBe("One Hundred Twenty Three");
 });
 
 test("20170", () => {
-  expect(numberToWords("20170")).toBe(
-    "Twenty Thousand One Hundred Seventy"
-  );
+  expect(numberToWords("20170")).toBe("Twenty Thousand One Hundred Seventy");
 });
 
 test("123.10", () => {
@@ -19,8 +17,72 @@ test("123.10", () => {
 });
 
 test("123.1", () => {
-  expect(amountToWords("123.1", 1)).toEqual({
+  expect(amountToWords(123.1, 1)).toEqual({
     decimalInWords: "One",
     numberInWords: "One Hundred Twenty Three",
   });
+});
+
+test("1234567890", () => {
+  expect(numberToWords(1234567890)).toBe("Overflow");
+});
+
+test("123456789", () => {
+  expect(numberToWords(123456789)).toBe(
+    "Twelve Crore Thirty Four Lakh Fifty Six Thousand Seven Hundred Eighty Nine"
+  );
+});
+
+test("NaN", () => {
+  expect(numberToWords(NaN)).toBe("NaN");
+});
+
+test("Zero", () => {
+  expect(numberToWords(0)).toBe("Zero");
+});
+
+test("1234567890", () => {
+  expect(numberToWords(1234567890)).toBe("Overflow");
+});
+
+test("010203405", () => {
+  expect(numberToWords(10203405)).toBe(
+    "One Crore Two Lakh Three Thousand Four Hundred Five"
+  );
+});
+
+test("060708910", () => {
+  expect(numberToWords("060708910")).toBe(
+    "Six Crore Seven Lakh Eight Thousand Nine Hundred Ten"
+  );
+});
+
+test("111213914", () => {
+  expect(numberToWords(111213914)).toBe(
+    "Eleven Crore Twelve Lakh Thirteen Thousand Nine Hundred Fourteen"
+  );
+});
+
+test("151617918", () => {
+  expect(numberToWords(151617918)).toBe(
+    "Fifteen Crore Sixteen Lakh Seventeen Thousand Nine Hundred Eighteen"
+  );
+});
+
+test("192030140", () => {
+  expect(numberToWords(192030140)).toBe(
+    "Nineteen Crore Twenty Lakh Thirty Thousand One Hundred Forty"
+  );
+});
+
+test("506070180", () => {
+  expect(numberToWords(506070180)).toBe(
+    "Fifty Crore Sixty Lakh Seventy Thousand One Hundred Eighty"
+  );
+});
+
+test("900000000", () => {
+  expect(numberToWords(900000000)).toBe(
+    "Ninety Crore"
+  );
 });
--- numberToWords.ts
+++ numberToWords.ts
@@ -1,24 +1,24 @@
 const lt20Words = [
   "",
-  "One ",
-  "Two ",
-  "Three ",
-  "Four ",
-  "Five ",
-  "Six ",
-  "Seven ",
-  "Eight ",
-  "Nine ",
-  "Ten ",
-  "Eleven ",
-  "Twelve ",
-  "Thirteen ",
-  "Fourteen ",
-  "Fifteen ",
-  "Sixteen ",
-  "Seventeen ",
-  "Eighteen ",
-  "Nineteen ",
+  "One",
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten",
+  "Eleven",
+  "Twelve",
+  "Thirteen",
+  "Fourteen",
+  "Fifteen",
+  "Sixteen",
+  "Seventeen",
+  "Eighteen",
+  "Nineteen",
 ];
 const get20Words = [
   "",
@@ -33,15 +33,18 @@ const get20Words = [
   "Ninety",
 ];
 
-const regex = /^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/;
-
-const getFirst20Words = (n: string) => lt20Words[Number(n)];
-const GetAfter20Words = (n: string) =>
-  get20Words[Number(n[0])] + " " + lt20Words[Number(n[1])];
+const getFirst99Words = (n: number) => {
+  const isLt20 = n < 20;
+  const tensPlace = Math.floor(n / 10);
+  const onesPlace = n % 10;
+  if (isLt20) return lt20Words[n];
+  if (onesPlace) return get20Words[tensPlace] + " " + lt20Words[onesPlace];
+  else return get20Words[tensPlace];
+};
 
 export const numberToWords = (input: string | number) => {
   const number = typeof input === "string" ? parseInt(input) : input;
-  if (isNaN(number)) return "";
+  if (isNaN(number)) return "NaN";
   if (number === 0) return "Zero";
 
   const numStr = number.toString();
@@ -49,28 +52,20 @@ export const numberToWords = (input: string | number) => {
     return "Overflow"; // Does not support converting more than 9 digits yet
   }
 
-  const [, n5, n4, n3, n2, n1] = numStr
-    .padStart(9, "000000000")
-    .match(regex) ?? ["00", "00", "00", "00", "00"]; // left pad zeros
-
-  // console.log({ n5, n4, n3, n2, n1 });
+  const numPadStr = numStr.padStart(9, "000000000");
+  const n1 = Number(numPadStr.slice(-2));
+  const n2 = Number(numPadStr.slice(-3, -2));
+  const n3 = Number(numPadStr.slice(-5, -3));
+  const n4 = Number(numPadStr.slice(-7, -5));
+  const n5 = Number(numPadStr.slice(-9, -7));
+  // console.log({ n5, n4, n3, n2, n1, numPadStr });
 
   let amountInWords = "";
-  amountInWords +=
-    Number(n5) != 0
-      ? (getFirst20Words(n5) || GetAfter20Words(n5)) + "Crore "
-      : "";
-  amountInWords +=
-    Number(n4) != 0
-      ? (getFirst20Words(n4) || GetAfter20Words(n4)) + "Lakh "
-      : "";
-  amountInWords +=
-    Number(n3) != 0
-      ? (getFirst20Words(n3) || GetAfter20Words(n3)) + "Thousand "
-      : "";
-  amountInWords += Number(n2) != 0 ? getFirst20Words(n2) + "Hundred " : "";
-  amountInWords +=
-    Number(n1) != 0 ? getFirst20Words(n1) || GetAfter20Words(n1) : "";
+  amountInWords += n5 !== 0 ? getFirst99Words(n5) + " Crore " : "";
+  amountInWords += n4 !== 0 ? getFirst99Words(n4) + " Lakh " : "";
+  amountInWords += n3 !== 0 ? getFirst99Words(n3) + " Thousand " : "";
+  amountInWords += n2 !== 0 ? getFirst99Words(n2) + " Hundred " : "";
+  amountInWords += n1 !== 0 ? getFirst99Words(n1) : "";
 
   return amountInWords.trim();
 };
